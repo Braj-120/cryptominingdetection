6e33a1b09f3160194e12d07dbc01702bc3235832
Braj K <braj.120@gmail.com>
2021-11-26 21:12:04 +0530

    Startup file

--- agent/startup.sh
+++ agent/startup.sh
@@ -1,1 +1,25 @@
+
+echo "This script works on Ubuntu and debian systems and used to install tshark utility before starting the program"
+echo "If you are on any other variant of linux, install tshark manually, then run following commands"
+echo "sudo chmod +x /usr/bin/dumpcap"
+echo "pip install -r requirements.txt"
+echo "python3 main.py"
+
+isPackageNotInstalled() {
+
+    dpkg --status $1 &> /dev/null
+    if [ $? -eq 0 ]; then
+    echo "$1: Already installed"
+    else
+    sudo apt-get install -y $1
+    sudo chmod +x /usr/bin/dumpcap
+    fi
+    }
+
+isPackageNotInstalled tshark
+
+echo "Installing Python dependencies"
+pip install -r requirements.txt
+
+echo "Starting"
 python3 main.py
